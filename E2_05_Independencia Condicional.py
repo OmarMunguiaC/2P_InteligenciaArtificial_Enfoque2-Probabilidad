8db5efb51350e33d8f89817af450b1e68ac3209e
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-05-02 17:10:03 -0600

    Independencia Condicional

---
+++ E2_05_Independencia Condicional.py
@@ -0,0 +1,9 @@
+# Definir dos eventos A y B
+evento_A = True
+evento_B = True
+
+# Verificar si los eventos A y B son independientes
+if evento_A and evento_B:
+    print("Los eventos A y B son independientes condicionalmente.")
+else:
+    print("Los eventos A y B no son independientes condicionalmente.")
