1c8373b238ba76a6fc5621e5b06fe2fc21ce99d1
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-05-02 17:09:41 -0600

    Distribucion de Probabilidad

---
+++ E2_04_Distribucion de Probabilidad.py
@@ -0,0 +1,4 @@
+# Definir la distribución de probabilidad para el lanzamiento de un dado justo
+distribucion_dado = {1: 1/6, 2: 1/6, 3: 1/6, 4: 1/6, 5: 1/6, 6: 1/6}
+
+print("Distribución de probabilidad para el lanzamiento de un dado justo:", distribucion_dado)
