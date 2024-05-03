09988bbc6e4283c708d4ce80332159df1b84c96f
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-05-02 20:06:03 -0600

    Aprendizaje Bayesiano

---
+++ E2_23_Aprendizaje Bayesiano.py
@@ -0,0 +1,20 @@
+import numpy as np
+import setuptools as pm
+
+# Datos observados
+datos = np.random.normal(loc=5, scale=2, size=100)
+
+# Definir el modelo bayesiano
+with pm.Model() as modelo:
+    # Parámetros priors
+    media = pm.Normal('media', mu=0, sigma=10)
+    sigma = pm.HalfNormal('sigma', sigma=10)
+
+    # Likelihood
+    observaciones = pm.Normal('observaciones', mu=media, sigma=sigma, observed=datos)
+
+    # Realizar muestreo MCMC
+    traza = pm.sample(1000, tune=1000)
+
+# Analizar resultados
+print(pm.summary(traza))
